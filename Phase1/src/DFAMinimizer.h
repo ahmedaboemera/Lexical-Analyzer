ab5c54fb7f925d0cc2281da0e31d7a616927df34
Ahmed Aboemera <ahmed.aboemera@badrit.com>
2015-03-15 20:10:41 +0200

    pulling

--- Phase1/src/DFAMinimizer.h
+++ Phase1/src/DFAMinimizer.h
@@ -18,8 +18,8 @@ class DFAMinimizer {
 public:
 	static void _minimize_dfa(DFA* in, DFA* out);
 private:
-	static void _reduce(map<int,map<int,pair<int,int> > >* in, DFA* out);
-	static void _build_map(DFA* in, map<int,map<int,pair<int,int> > >* out);
+	static void _reduce(map<int,map<int,set<pair<int,int> > > >* in, DFA* out);
+	static void _build_map(DFA* in, map<int,map<int,set<pair<int,int> > > >* out);
 };
 
 #endif /* SRC_DFAMINIMIZER_H_ */
